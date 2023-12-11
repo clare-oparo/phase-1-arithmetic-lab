28d0b4d960dac64e78b2030936e7ee960f2055dc
Clare <clare.oparo@student.moringaschool.com>
2023-12-11 15:15:49 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 25;
+const num4 = 7;
+const mod = num3%num4;
+
+const max = Math.max(3, 4, 7,8, 20);
